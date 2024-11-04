855b929aa38e5c3d6338c96c4795b96cff1052c6
Vladyslav Simak <simakv21@gmail.com>
2024-11-04 12:37:36 +0200

    fix comp

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.getElementById('compareButton').addEventListener('click', function() {
     const array1 = document.getElementById('array1').value.split(',').map(item => item.trim());
     const array2 = document.getElementById('array2').value.split(',').map(item => item.trim());
 
-    const commonCountries = array1.filter(country => array2.includes(country));
+    const commonCountries = array1.filter(country => !array2.includes(country));
 
     const resultElement = document.getElementById('result');
     resultElement.innerHTML = '';
